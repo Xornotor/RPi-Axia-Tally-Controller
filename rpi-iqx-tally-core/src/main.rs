1cfe95e7c3bb69e54b4ae3676cf9441730db257c
And Paiva <andre.pcr@hotmail.com>
2024-03-18 22:27:11 -0300

    JSON Handler: Write_to_json and init_tally_config

--- rpi-iqx-tally-core/src/main.rs
+++ rpi-iqx-tally-core/src/main.rs
@@ -1,23 +1,9 @@
 mod json_handler;
 
-use serde_json;
-use std::{fs, io::Write};
+//use std::thread;
 use crate::json_handler::*;
 
 fn main() {
-    let tally_cfg = TallyConfig::standard();
-
-    let serialized = tally_cfg.to_json().unwrap();
-
-    println!("serialized: \n{}", serialized);
-
-    let deserialized: TallyConfig = serde_json::from_str(&serialized).unwrap();
-
-    println!("deserialized: \n{:?}", deserialized);
-
-    let mut f = fs::File::create("tally_config.json").expect("Failed to create");
-
-    f.write_all(&serialized.as_bytes()).expect("Failed to write");
-
-    drop(f);
+    let tally_cfg = init_tally_config().expect("Error accessing tally config file");
+    println!("{:?}", tally_cfg);
 }
