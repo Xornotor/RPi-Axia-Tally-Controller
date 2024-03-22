5e0f4ee5ac055667b50fd5e64d8fdd9f7fb97c41
And Paiva <andre.pcr@hotmail.com>
2024-03-22 13:24:03 -0300

    Fully functioning API (restart/recfg)

--- rpi-iqx-tally-core/src/main.rs
+++ rpi-iqx-tally-core/src/main.rs
@@ -9,6 +9,7 @@ use crate::api_handler::*;
 use crate::ctrl_thread::*;
 
 use actix_web::*;
+use actix_cors::Cors;
 //use std::sync::{Arc, mpsc::{Sender}};
 
 #[actix_web::main]
@@ -19,7 +20,9 @@ async fn main() -> std::io::Result<()> {
     let actix_data = web::Data::new(data);
     
     HttpServer::new(move || {
+    	let cors = Cors::permissive();
 		App::new()
+			.wrap(cors)
 			.app_data(web::Data::clone(&actix_data))
 			.service(restart)
 			.service(reconfig)
